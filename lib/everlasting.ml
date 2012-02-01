bc5bac2e274f043b40e31e7ef7e2a626a3e2700a
Hugo Heuzard <hugo.heuzard@gmail.com>
2012-01-31 20:05:19 -0800

    add clear

--- lib/everlasting.ml
+++ lib/everlasting.ml
@@ -1,27 +1,28 @@
 
 type 'a t
-(*      
+(*
 external create : int -> 'a t = "ocaml_create"
 
 
 (* when calling set, the value shouldn't have been created in the call *)
 
-external set : 'a t -> int -> 'a -> unit = "ocaml_set"  
+external set : 'a t -> int -> 'a -> unit = "ocaml_set"
 external get : 'a t -> int -> 'a  = "ocaml_get"
 external replace : 'a t -> int -> 'a -> unit = "ocaml_replace"
 
 
 (** copy an ocaml element outside of the heap, to use with some care .. *)
-external copy : 'a -> 'a = "ocaml_copy" 
+external copy : 'a -> 'a = "ocaml_copy"
 (** update an element outside of the heap with an element inside the heap *)
-external update : 'a -> 'a -> 'a = "ocaml_update"    
-*)   
+external update : 'a -> 'a -> 'a = "ocaml_update"
+*)
 
 
-let create n = Hashtbl.create n 
-let set = Hashtbl.add 
+let create n = Hashtbl.create n
+let set = Hashtbl.add
 let get = Hashtbl.find
 
+let clear = Hashtbl.clear
 let replace = Hashtbl.replace
 let remove = Hashtbl.remove
 
